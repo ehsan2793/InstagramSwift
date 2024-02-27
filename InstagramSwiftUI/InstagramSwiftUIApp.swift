f34e6661be8c53ca2a3420db43717731ead08168
ehsan2793 <ehsan2793@gmail.com>
2024-02-26 17:54:06 -0800

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Ehsan Rahimi on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
