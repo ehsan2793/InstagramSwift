94158ba030c8849f928891e945e68b981ee58d82
ehsan2793 <ehsan2793@gmail.com>
2024-03-10 12:12:46 -0700

    started adding firebase

--- InstagramSwiftUI/InstagramSwiftUIApp.swift
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -5,10 +5,15 @@
 //  Created by Ehsan Rahimi on 2/26/24.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct InstagramSwiftUIApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
